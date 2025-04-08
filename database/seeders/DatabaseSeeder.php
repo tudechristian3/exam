e0dc38320dd489c37e3f64852ca15396a234ee64
Christian Neil Tude <tudechristian3@gmail.com>
2025-04-08 17:40:05 +0800

    done creating user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\ProductsSeeder;
 use Database\Seeders\TaskSeeder;
+use Database\Seeders\UserSeeder;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,15 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('admin123')
-        ]);
-
         $this->call([
+            UserSeeder::class,
             ProductsSeeder::class,
             TaskSeeder::class,
         ]);
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        User::create([
+            'id' => 1,
+            'name' => "Test User",
+            'email' => "test@example.com",
+            'password' => Hash::make('admin123')
+        ]);
+
+        foreach (range(1, 10) as $index) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password123'), // or generate a random password
+            ]);
+        }
+    }
+}
